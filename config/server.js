8f6847899b7cfa3ce6f05af3ef22478da50901af
Gabriele Napoli <gabrielenapoli@MBP-di-Gabriele.station>
2020-12-12 18:11:07 +0100

    Initial commit con DB

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1008a89c84ce33d72676af0281ffb07b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '42821b51-7f1d-434a-9ad4-2d3a24b3eded'
+};
