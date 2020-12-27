2d5446c55ba3edce20ced71dc29d3cf8993f3b31
Gabriele Napoli <gabrielenapoli@MacBook-Pro-di-Gabriele.local>
2020-12-27 17:38:29 +0100

    add custom controller to profiles

--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -1,8 +1,29 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
 
-module.exports = {};
+  async find(ctx) {
+    console.log(ctx)
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.profile.search(ctx.query);
+    } else {
+      entities = await strapi.services.profile.find(ctx.query);
+    }
+
+    //Check if users has paid to see info about devs
+    entities.map(entity => {
+        entity.name = "Name"
+        entity.location = "Location"
+        entity.preferred_salary = "Salary"
+        entity.hasPaid = false
+    })
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.profile }));
+  },
+};
